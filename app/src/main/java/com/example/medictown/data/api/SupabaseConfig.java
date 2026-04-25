1af0816ff7b109fc143079f117b3974c9c3ba275
Lộc Trần Quý <24520987@ms.uit.edu.vn>
2026-04-25 19:59:09 +0700

    thiết kế giao diện đăng nhập, đăng ký

---
+++ app/src/main/java/com/example/medictown/data/api/SupabaseConfig.java
@@ -0,0 +1,7 @@
+package com.example.medictown.data.api;
+
+public class SupabaseConfig {
+    // Thay thế bằng thông tin từ Project Settings -> API trên Dashboard Supabase của bạn
+    public static final String SUPABASE_URL = "https://wuhoqresirnxjnxulviw.supabase.co/rest/v1/";
+    public static final String SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Ind1aG9xcmVzaXJueGpueHVsdml3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY5ODE0NTQsImV4cCI6MjA5MjU1NzQ1NH0.AwgRTbanQTvW7UxZld3GO4dXSKn6qdBT2mf5A1NDMJs";
+}
